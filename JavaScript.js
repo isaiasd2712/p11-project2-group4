0633df21dc43bca530e037753baa39d03742ea95
isaiasd2712 <isaiasd2712@gmail.com>
2023-10-05 15:47:48 +0200

    gif added

--- JavaScript.js
+++ JavaScript.js
@@ -2,11 +2,22 @@ let names= ["Isaias", "Karol", "Andrés", "Seif"];
 
 // set name list
 
+// cuando hagamos click en #pulsador
 document.getElementById("startbutton").onclick= function (){
+    
+    let name;
+    //miramos si la lista está vacía
+    if(names.length ===0) {
+       name= "Game Over"
+    } else {
+    // Sacamos un número al azar
     let number= Math.floor(Math.random() * names.length);
     console.log("Ha salido el numero" + number);
-    let name = names[number];
+    // sacamos un nombre y lo quitamos de la lista
+    name = names.splice(number,1)[0];
     console.log("El número" + number + "corresponde a " + name);
-
-    document.getElementBy
+    }
+    
+    //Pintamos la variable donde antes ponía ´´Cargando´
+    document.getElementById("pantalla"),innerHtml = name;
 }
