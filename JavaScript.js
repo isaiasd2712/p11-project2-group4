395a5c6ee290131300360590b10bbb906fbf45e0
isaiasd2712 <isaiasd2712@gmail.com>
2023-10-06 12:51:39 +0200

    nombres aleatorios

--- JavaScript.js
+++ JavaScript.js
@@ -1,23 +1,5 @@
-let names= ["Isaias", "Karol", "Andrés", "Seif"];
-
-// set name list
-
-// cuando hagamos click en #pulsador
-document.getElementById("startbutton").onclick= function (){
-    
-    let name;
-    //miramos si la lista está vacía
-    if(names.length ===0) {
-       name= "Game Over"
-    } else {
-    // Sacamos un número al azar
+function generate() {
+    let names= ["Isaias", "Karol", "Andrés", "Seif"];
     let number= Math.floor(Math.random() * names.length);
-    console.log("Ha salido el numero" + number);
-    // sacamos un nombre y lo quitamos de la lista
-    name = names.splice(number,1)[0];
-    console.log("El número" + number + "corresponde a " + name);
-    }
-    
-    //Pintamos la variable donde antes ponía ´´Cargando´
-    document.getElementById("pantalla"),innerHtml = name;
+    document.getElementById('pantalla').innerHTML = "<h1>Nombre:</h1><div class='alert alert-success'><h2>" + names[number] + "</h2></div>";
 }
